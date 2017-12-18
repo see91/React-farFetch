a0ccbe3db6a880a73b8fe281e71714d5a391338f
zhangzengjin1213 <751789253@qq.com>
2017-12-19 01:21:13 +0800

    愿望单 用户个人中心等页面搭建

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,10 @@ import Brand from "./components/Brand/index";
 import WishList from "./components/WishList/index";
 import './pubComponents/style/tab.css'
 import Tab from "./pubComponents/Tab/index";
-
+import Settlement from './components/Settlement/index'
+import Harvest from './components/Harvest/index';
+import ShoppingCars from './components/ShoppingCars/index'
+import Login from './components/Login/index';
 export default class Routes extends Component {
     render() {
         return (
@@ -22,6 +25,10 @@ export default class Routes extends Component {
                         <Route path="/brand" exact component={Brand}/>
                         <Route path="/wishList" exact component={WishList}/>
                         <Route path="/user" component={User}/>
+                        <Route path="/settlement" component={Settlement}/>
+                        <Route path="/harvest" component={Harvest}/>
+                        <Route path="/shoppingCars" component={ShoppingCars}/>
+                        <Route path="/login" component={Login}/>
                     </Switch>
                 </div>
             </Router>

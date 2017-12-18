4e329d46caaa4cacf1b082a1dbf810058ff6977e
zhouyinghao <541218502@qq.com>
2017-12-19 00:32:16 +0800

    列表样式修改

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -5,9 +5,20 @@ import MHeader from '../../pubComponents/MHeader'
 import './index.less'
 import {Link} from 'react-router-dom'
 class List extends Component{
+    constructor(){
+        super();
+        this.state={showList:false}
+    }
     componentDidMount() {
         this.props.getDatas()
     }
+    switchShow = () => {
+        this.setState({showList: !this.state.showList});
+    }
+    // changeType=(event)=>{
+    //     let event=event.target.dataset.value;
+    //     this.props.changeType(type)
+    // }
     render(){
         console.log(this.props.getList);
         return (
@@ -21,23 +32,31 @@ class List extends Component{
                     <div className="container">
 
                         <h3>SALE|近4000件商品现有4折优惠</h3>
+                        <div className="list-group">
+                        {
 
-                            {
-                                this.props.getList.map((item,index)=>(
-                                    <Link key={item.id} to={{pathname: `/detail/${item.id}`, state: {item}}}>
-                                        <div className="list">
-                                            <img src={item.url}/>
-                                            <p>{item.title}</p>
-                                            <b>￥{item.price}</b>
-                                        </div>
-                                    </Link>
-                                ))
-                            }
+                            this.props.getList.map((item,index)=>(
+                                <Link  key={item.id} to={{pathname: `/detail/${item.id}`, state: {item}}}>
+                                    <div onClick={this.showList} className="iconfont icon-gray-star">
+                                        {/*{*/}
+                                        {/*this.state.showList?<div className="iconfont icon-gray-star"></div>:<div className="iconfont icon-gray-star"></div>*/}
+                                        {/*}*/}
+                                    </div>
+                                   <div onClick={this.switchShow}>
 
+                                   </div>
+                                        <img src={item.url}/>
+                                        <p>{item.title}</p>
+                                        <b>￥{item.price}</b>
+                                </Link>
+                            ))
+                        }
+                            </div>
+                        </div>
                     </div>
 
                 </div>
-            </div>
+
         )
     }
 }

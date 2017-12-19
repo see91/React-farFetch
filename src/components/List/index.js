610ac4f412a3cf07b1d0020206712faf298dc4b3
see91 <loverrenmin@qq.com>
2017-12-19 13:14:41 +0800

    ......

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,17 +1,20 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux'
 import actions from '../../store/action/list';
 import MHeader from '../../pubComponents/MHeader'
 import './index.less'
 import {Link} from 'react-router-dom'
-class List extends Component{
-    constructor(){
+
+class List extends Component {
+    constructor() {
         super();
-        this.state={showList:false}
+        this.state = {showList: false}
     }
+
     componentDidMount() {
         this.props.getDatas()
     }
+
     switchShow = () => {
         this.setState({showList: !this.state.showList});
     }
@@ -19,7 +22,7 @@ class List extends Component{
     //     let event=event.target.dataset.value;
     //     this.props.changeType(type)
     // }
-    render(){
+    render() {
         console.log(this.props.getList);
         return (
             <div>
@@ -33,34 +36,35 @@ class List extends Component{
 
                         <h3>SALE|近4000件商品现有4折优惠</h3>
                         <div className="list-group">
-                        {
+                            {
 
-                            this.props.getList.map((item,index)=>(
-                                <Link  key={item.id} to={{pathname: `/detail/${item.id}`, state: {item}}}>
-                                    <div onClick={this.showList} className="iconfont icon-gray-star">
-                                        {/*{*/}
-                                        {/*this.state.showList?<div className="iconfont icon-gray-star"></div>:<div className="iconfont icon-gray-star"></div>*/}
-                                        {/*}*/}
-                                    </div>
-                                   <div onClick={this.switchShow}>
+                                this.props.getList.map((item, index) => (
+                                    <Link key={item.id} to={{pathname: `/detail/${item.id}`, state: {item}}}>
+                                        <div onClick={this.showList} className="iconfont icon-gray-star">
+                                            {/*{*/}
+                                            {/*this.state.showList?<div className="iconfont icon-gray-star"></div>:<div className="iconfont icon-gray-star"></div>*/}
+                                            {/*}*/}
+                                        </div>
+                                        <div onClick={this.switchShow}>
 
-                                   </div>
+                                        </div>
                                         <img src={item.url}/>
                                         <p>{item.title}</p>
                                         <b>￥{item.price}</b>
-                                </Link>
-                            ))
-                        }
-                            </div>
+                                    </Link>
+                                ))
+                            }
                         </div>
                     </div>
-
                 </div>
+
+            </div>
 
         )
     }
 }
+
 export default connect(
-    state=>state.list,
+    state => state.list,
     actions
 )(List)

34bd43d576a5a5d8d01162a3348b59e0ca1fb5cb
see91 <loverrenmin@qq.com>
2017-12-20 11:50:46 +0800

    修改Tab 样式露层问题

--- src/components/Home/Shop/index.js
+++ src/components/Home/Shop/index.js
@@ -1,20 +1,21 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import MHeader from "../../../pubComponents/MHeader/index";
 import './index.less'
-export default class Shop extends Component{
-    render(){
+
+export default class Shop extends Component {
+    render() {
         return (
             <div className="my-shop">
-              <div className="top">
+                <div className="top">
 
-              </div>
-              <div className="middle">
-                购物袋中暂无商品
-              </div>
-              <div className="bottom">
+                </div>
+                <div className="middle">
+                    购物袋中暂无商品
+                </div>
+                <div className="bottom">
 
-              </div>
-              <button>立即选购</button>
+                </div>
+                <button>立即选购</button>
             </div>
         )
     }

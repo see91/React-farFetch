15ba43a49fae59f94bc0534172c01fc593120031
zhangzengjin1213 <751789253@qq.com>
2017-12-20 15:36:22 +0800

    login

--- src/components/ShoppingCars/index.js
+++ src/components/ShoppingCars/index.js
@@ -13,7 +13,7 @@ export default class ShoppingCars extends Component {
     }
     render() {
         return (
-            <div id="shopping">
+            <div className="shopping">
                 <MHeader title={{title: '购物袋'}} back={true}/>
                 <div className='shopcarContent'>
                     <ul>
@@ -26,21 +26,25 @@ export default class ShoppingCars extends Component {
                           <p className="THOM">THOMBROWNE</p>
                           <p className="Numbering">:12423301</p>
                             <button className="number">数量1</button>
-                          <span className="Join">☆加入愿望单</span>
-                          <span className="unitPrice">¥5,200.00</span>
+                          <p className="Join">☆加入愿望单</p>
+                          <p className="unitPrice">¥5,200.00</p>
                         </li>
-                      
-
-
-
-
-
-
-
-
-
-
-
+                        <li className="Products">
+                            <h5 className="Origins">一项商品产地来自美国</h5>
+                            <p className="THOM">THOMBROWNE</p>
+                            <p className="Numbering">:12423301</p>
+                            <button className="number">数量1</button>
+                            <p className="Join">☆加入愿望单</p>
+                            <p className="unitPrice">¥5,200.00</p>
+                        </li>
+                        <li className="Products">
+                            <h5 className="Origins">一项商品产地来自美国</h5>
+                            <p className="THOM">THOMBROWNE</p>
+                            <p className="Numbering">:12423301</p>
+                            <button className="number">数量1</button>
+                            <p className="Join">☆加入愿望单</p>
+                            <p className="unitPrice">¥5,200.00</p>
+                        </li>
                         <li className="one">
                             <h4>14天无理由免费退货(退款含税)</h4>
                             <p>还在犹豫？别担心，我们提供14天无理由年费退货。</p>
@@ -48,7 +52,7 @@ export default class ShoppingCars extends Component {
                         </li>
                         <li className="help">
                             <h3>需要帮助?</h3>
-                            <p>请以如下方式联系我们的客服：</p>
+                            <p>请以如下方式联系我们的客服:</p>
                             <button className='iconfont icon-dianhua iphone'></button>
                             <button className='iconfont icon-youxiang email'></button>
                         </li>
@@ -59,7 +63,6 @@ export default class ShoppingCars extends Component {
                         >去结算</button>
                     </ul>
                 </div>
-
             </div>
         )
     }

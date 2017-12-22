aa5dbdb26bf34027e2c10e28908e20ec8459fe84
siaohu1 <87170508@qq.com>
2017-12-23 00:10:53 +0800

    1

--- src/components/Home/Loading-db/index.js
+++ src/components/Home/Loading-db/index.js
@@ -1,13 +1,14 @@
 import React,{Component} from 'react';
-import './index.less'
+import './index.less';
+import '/Users/tiger1/Desktop/React-farFetch/src/components/Home/Loading-db1/animate.css';
 export default class Loading extends Component{
     handleLoad(){
-      window.setTimeout("window.location='/#/home'",3000);
+      window.setTimeout("window.location='/#/home'",4000);
     }
     render(){
         return (
             <div>
-              <img src={require("../../../static/img/loading.png")} className="loading-db" onLoad={this.handleLoad}/>
+              <img src={require("../../../static/img/loading.png")} className="loading-db animated-db1 flash" onLoad={this.handleLoad}/>
             </div>
         )
     }
